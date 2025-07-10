18f9be8d2f687b026d68aff5c81d72a357376e54
Mrcheeseman2 <caydenamiller@gmail.com>
2025-07-10 14:03:22 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -1,5 +1,14 @@
 var stemclass = "Technology";
+let myName = "Cayden";
 
 console.log(`This code was written in the STEM PREP: ${stemclass} class`);
 
-alert("This is an alert!");
+alert(`This code was written in the STEM PREP: ${stemclass} class by ${myName}`);
+
+var name = prompt("Hello, what is your name?");
+var school = prompt("What school do you attend? ");
+var grade = prompt("What is the grade you are in? ");
+
+document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
+document.getElementById("school").innerHTML = "School: <b>" + school + "</b>";
+document.getElementById("grade").innerHTML = "grade: <b>" + grade + "</b>";
