f993291c62f11d99d505cadd4c207774180540fb
Mrcheeseman2 <caydenamiller@gmail.com>
2025-07-15 14:49:25 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -9,3 +9,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let centralMarker = L.marker([35.2176665, -80.831473], {title: "CPCC Central Campus"}).addTo(map);
+let centralMarker = L.marker([35.2648627, -80.73116], {title: "CPCC Cato Campus"}).addTo(map);
+let centralMarker = L.marker([35.173847, -80.922945], {title: "CPCC Harper Campus"}).addTo(map);
+let centralMarker = L.marker([35.227085, --80.843124], {title: "CPCC Harris Campus"}).addTo(map);
+let centralMarker = L.marker([35.102469, -80.709380], {title: "CPCC Levine Campus"}).addTo(map);
+let centralMarker = L.marker([35.4332997, --80.852924 ], {title: "CPCC Merancas Campus"}).addTo(map);
