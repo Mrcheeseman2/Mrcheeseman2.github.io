d18d8096eb9ac2cec759a9e70e7248b11dea3918
Mrcheeseman2 <caydenamiller@gmail.com>
2025-07-17 13:14:01 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -11,6 +11,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let centralMarker = L.marker([35.2176665, -80.831473], {title: "CPCC Central Campus"}).addTo(map);
 let catoMarker = L.marker([35.2648627, -80.73116], {title: "CPCC Cato Campus"}).addTo(map);
 let harperMarker = L.marker([35.173847, -80.922945], {title: "CPCC Harper Campus"}).addTo(map);
-let harrisMarker = L.marker([35.227085, -80.843124], {title: "CPCC Harris Campus"}).addTo(map);
+let harrisMarker = L.marker([35.22, -80.92], {title: "CPCC Harris Campus"}).addTo(map);
 let levineMarker = L.marker([35.102469, -80.709380], {title: "CPCC Levine Campus"}).addTo(map);
 let merancasMarker = L.marker([35.4332997, -80.852924 ], {title: "CPCC Merancas Campus"}).addTo(map);
