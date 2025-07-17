6eb18a3df539537c099092671f362c6f1f2a4c6c
Mrcheeseman2 <caydenamiller@gmail.com>
2025-07-17 13:28:42 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -13,4 +13,5 @@ let catoMarker = L.marker([35.2648627, -80.73116], {title: "CPCC Cato Campus"}).
 let harperMarker = L.marker([35.123847, -80.922945], {title: "CPCC Harper Campus"}).bindPopup("<p>Harper Campus</p>").addTo(map);
 let harrisMarker = L.marker([35.22, -80.92], {title: "CPCC Harris Campus"}).bindPopup("<p>Harris Campus</p>").addTo(map);
 let levineMarker = L.marker([35.102469, -80.709380], {title: "CPCC Levine Campus"}).bindPopup("<p>Levine Campus</p>").addTo(map);
-let merancasMarker = L.marker([35.4332997, -80.852924 ], {title: "CPCC Merancas Campus"}).bindPopup("<p>Merancas Campus</p>").addTo(map);
+let merancasMarker = L.marker([35.4332997, -80.852924], {title: "CPCC Merancas Campus"}).bindPopup("<p>Merancas Campus</p>").addTo(map);
+let tokyoMarker = L.marker([35.652832, 139.839478], {title: "Tokyo"}).bindPopup("<p>Tokyo</p>").addTo(map);
